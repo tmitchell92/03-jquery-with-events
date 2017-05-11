209c1e57c1f79308ccc62f66088c3d1ac8afccea
Ryan Turner <ryanthepit@gmail.com>
2017-05-11 16:39:32 -0700

    deleted console logs

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -39,7 +39,6 @@ articleView.handleAuthorFilter = function() {
     if ($(this).val()) {
 
       var $authorName = $(this).val()
-      console.log($authorName)
       $('article').hide();
       $('article[data-author="' + $authorName + '"]').fadeIn(700)
 
@@ -63,7 +62,6 @@ articleView.handleCategoryFilter = function() {
   $('#category-filter').on('change', function() {
     if ($(this).val()) {
       var $categoryName = $(this).val()
-      console.log($categoryName)
       $('article').hide();
       $('article[data-category="' + $categoryName + '"]').fadeIn(700);
 
@@ -78,7 +76,7 @@ articleView.handleCategoryFilter = function() {
   });
 };
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  // as: Add an event handler to .main-nav elements that will power the Tabs feature.
   //       Clicking any .tab element should hide all the .tab-content sections, and then reveal the
   //       single .tab-content section that is associated with the clicked .tab element.
   //       So: You need to dynamically build a selector string with the correct ID, based on the
